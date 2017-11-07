bef67ddf8b390305af1ae91969674079ed2fe676
Mathias Storm <mathiasstorm1@gmail.com>
2017-11-07 22:14:03 +0100

    Adding ability to track experiences(still has test stuff in it)

---
+++ CodeStats.Client/CommandFilter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Text.Editor;
+using System.Windows.Input;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.Text;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+
+namespace CodeStats.Client
+{
+    class CommandFilter : IOleCommandTarget
+    {
+        private IWpfTextView _textView;
+        internal IOleCommandTarget NextTarget { get; set; }
+        internal bool Added { get; set; }
+        private IAdornmentLayer _adornmentLayer;
+
+        internal Dictionary<string, int> Experiences = new Dictionary<string, int>();
+        private Task _publisher = null;
+
+
+        public CommandFilter(IWpfTextView textView)
+        {
+            _textView = textView;
+            _adornmentLayer = _textView.GetAdornmentLayer("CodeStatsClientLayer");
+        }
+
+        int IOleCommandTarget.Exec(ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
+        {
+            if (nCmdID != (uint)VSConstants.VSStd2KCmdID.TYPECHAR && nCmdID != (uint)VSConstants.VSStd2KCmdID.BACKSPACE)
+                return NextTarget.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
+
+            incrementOrAdd(_textView.TextBuffer.ContentType.DisplayName);
+            MessageBox.Show("Added one to: " + _textView.TextBuffer.ContentType.DisplayName);
+
+            if (_publisher == null)
+            {
+                _publisher = Task.Factory.StartNew(() =>
+                {
+                    Thread.Sleep(10 * 1000); // Hopefully 10 seconds?
+
+                    string experiences = "";
+                    foreach (var experience in Experiences)
+                    {
+                        experiences += $"In Language {experience.Key}, you earned {experience.Value} xp\n";
+                    }
+                    MessageBox.Show(experiences);
+                    Experiences.Clear();
+                    _publisher = null;
+                });
+            }
+
+            return NextTarget.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
+        }
+
+        private void incrementOrAdd(string lang)
+        {
+            if (Experiences.ContainsKey(lang))
+            {
+                Experiences[lang]++;
+            }
+            else
+            {
+                Experiences.Add(lang, 1);
+            }
+        }
+
+        int IOleCommandTarget.QueryStatus(ref Guid pguidCmdGroup, uint cCmds, OLECMD[] prgCmds, IntPtr pCmdText)
+        {
+            return NextTarget.QueryStatus(ref pguidCmdGroup, cCmds, prgCmds, pCmdText);
+        }
+    }
+
+}
---
+++ CodeStats.Client/FilterProvider.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace CodeStats.Client
+{
+    [Export(typeof(IVsTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Editable)]
+    internal class FilterProvider : IVsTextViewCreationListener
+    {
+        [Export(typeof(AdornmentLayerDefinition))]
+        [Name("CodeStatsClientLayer")]
+        [TextViewRole(PredefinedTextViewRoles.Editable)]
+        internal AdornmentLayerDefinition m_multieditAdornmentLayer = null;
+
+        [Import(typeof(IVsEditorAdaptersFactoryService))]
+        internal IVsEditorAdaptersFactoryService editorFactory = null;
+
+        public void VsTextViewCreated(IVsTextView textViewAdapter)
+        {
+            IWpfTextView textView = editorFactory.GetWpfTextView(textViewAdapter);
+            if (textView == null)
+                return;
+
+            AddCommandFilter(textViewAdapter, new CommandFilter(textView));
+        }
+
+        void AddCommandFilter(IVsTextView viewAdapter, CommandFilter commandFilter)
+        {
+            if (commandFilter.Added == false)
+            {
+                //get the view adapter from the editor factory
+                IOleCommandTarget next;
+                int hr = viewAdapter.AddCommandFilter(commandFilter, out next);
+
+                if (hr == VSConstants.S_OK)
+                {
+                    commandFilter.Added = true;
+                    //you'll need the next target for Exec and QueryStatus
+                    if (next != null)
+                        commandFilter.NextTarget = next;
+                }
+            }
+        }
+
+    }
+}
